afbf3fd9e4f35b8f3306a705a23d28edacb986c3
DillonEnge <dillon.enge@gmail.com>
2018-11-17 16:55:45 -0500

    Modified homepage and built.

---
+++ build/precache-manifest.255871ce9d0a4fd88546666982982067.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1a2f4d4aa46886c850",
+    "url": "/static/css/main.58e6737e.chunk.css"
+  },
+  {
+    "revision": "bc1a2f4d4aa46886c850",
+    "url": "/static/js/main.bc1a2f4d.chunk.js"
+  },
+  {
+    "revision": "c22ca2d525d117050892",
+    "url": "/static/js/1.c22ca2d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "202a9779a51fb014740b6b6566b67a1d",
+    "url": "/static/media/Kellie1.202a9779.JPG"
+  },
+  {
+    "revision": "af204d3de1867d611ea78916de0dcfc3",
+    "url": "/static/media/Kellie2.af204d3d.JPG"
+  },
+  {
+    "revision": "8b1683ff8d12a5f49fc2e370b10bbd88",
+    "url": "/static/media/Kellie3.8b1683ff.JPG"
+  },
+  {
+    "revision": "5279e2abacafa61ce2923b5d8807f0da",
+    "url": "/static/media/Kellie4.5279e2ab.JPG"
+  },
+  {
+    "revision": "0e202b03d70128bc547835c8ee05b269",
+    "url": "/static/media/Kellie5.0e202b03.JPG"
+  },
+  {
+    "revision": "3e0d3d4e8f578b9fc332bde4a650ee54",
+    "url": "/static/media/Kellie6.3e0d3d4e.JPG"
+  },
+  {
+    "revision": "915ecf96ebb9044ed3327819e5594989",
+    "url": "/static/media/Kellie7.915ecf96.JPG"
+  },
+  {
+    "revision": "f363a7910ad2ac532247713f4f8fbfc6",
+    "url": "/static/media/Kellie8.f363a791.JPG"
+  },
+  {
+    "revision": "1d6df262d04579629b48041fcb26fa13",
+    "url": "/static/media/Kellie9.1d6df262.jpeg"
+  },
+  {
+    "revision": "047eaf9f23c8aab187136f640e723fc3",
+    "url": "/static/media/Kellie10.047eaf9f.jpeg"
+  },
+  {
+    "revision": "bf26484cb55d75f7f17af7909eab70f8",
+    "url": "/static/media/Kellie11.bf26484c.jpeg"
+  },
+  {
+    "revision": "98630f3c1edd9502caeaef0a80746133",
+    "url": "/static/media/KellieLogo.98630f3c.png"
+  },
+  {
+    "revision": "4789c311ddba633e4b3eecaedf469f74",
+    "url": "/index.html"
+  }
+];
