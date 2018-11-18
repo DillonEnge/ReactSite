a4b82695d019b2b2dbfc32761de453fae5525383
DillonEnge <dillon.enge@gmail.com>
2018-11-17 23:02:09 -0500

    Updates

---
+++ precache-manifest.cf191ae400c321ad6bd2e53062924f03.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff69d11063d2c2411d0",
+    "url": "/kellie-art-client/static/css/main.58e6737e.chunk.css"
+  },
+  {
+    "revision": "aff69d11063d2c2411d0",
+    "url": "/kellie-art-client/static/js/main.aff69d11.chunk.js"
+  },
+  {
+    "revision": "c22ca2d525d117050892",
+    "url": "/kellie-art-client/static/js/1.c22ca2d5.chunk.js"
+  },
+  {
+    "revision": "d4430ef4dab55f29372b",
+    "url": "/kellie-art-client/static/js/runtime~main.d4430ef4.js"
+  },
+  {
+    "revision": "202a9779a51fb014740b6b6566b67a1d",
+    "url": "/kellie-art-client/static/media/Kellie1.202a9779.JPG"
+  },
+  {
+    "revision": "af204d3de1867d611ea78916de0dcfc3",
+    "url": "/kellie-art-client/static/media/Kellie2.af204d3d.JPG"
+  },
+  {
+    "revision": "8b1683ff8d12a5f49fc2e370b10bbd88",
+    "url": "/kellie-art-client/static/media/Kellie3.8b1683ff.JPG"
+  },
+  {
+    "revision": "5279e2abacafa61ce2923b5d8807f0da",
+    "url": "/kellie-art-client/static/media/Kellie4.5279e2ab.JPG"
+  },
+  {
+    "revision": "0e202b03d70128bc547835c8ee05b269",
+    "url": "/kellie-art-client/static/media/Kellie5.0e202b03.JPG"
+  },
+  {
+    "revision": "3e0d3d4e8f578b9fc332bde4a650ee54",
+    "url": "/kellie-art-client/static/media/Kellie6.3e0d3d4e.JPG"
+  },
+  {
+    "revision": "915ecf96ebb9044ed3327819e5594989",
+    "url": "/kellie-art-client/static/media/Kellie7.915ecf96.JPG"
+  },
+  {
+    "revision": "f363a7910ad2ac532247713f4f8fbfc6",
+    "url": "/kellie-art-client/static/media/Kellie8.f363a791.JPG"
+  },
+  {
+    "revision": "1d6df262d04579629b48041fcb26fa13",
+    "url": "/kellie-art-client/static/media/Kellie9.1d6df262.jpeg"
+  },
+  {
+    "revision": "047eaf9f23c8aab187136f640e723fc3",
+    "url": "/kellie-art-client/static/media/Kellie10.047eaf9f.jpeg"
+  },
+  {
+    "revision": "bf26484cb55d75f7f17af7909eab70f8",
+    "url": "/kellie-art-client/static/media/Kellie11.bf26484c.jpeg"
+  },
+  {
+    "revision": "98630f3c1edd9502caeaef0a80746133",
+    "url": "/kellie-art-client/static/media/KellieLogo.98630f3c.png"
+  },
+  {
+    "revision": "fd1fa4e0e235c5b755ac8dbd58784760",
+    "url": "/kellie-art-client/index.html"
+  }
+];
